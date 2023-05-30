026f07288e496d3f5f922bdd0fc05de963e74bbb
DJ_Kovrik <djkovrik@gmail.com>
2023-05-30 20:04:30 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+## Blink Tracker
+Tracks how many times you blink per minute and notifies you if it's below defined threshold.
---
+++ app/build.gradle.kts
@@ -0,0 +1,70 @@
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    alias(libs.plugins.com.android.application)
+    alias(libs.plugins.org.jetbrains.kotlin.android)
+}
+
+android {
+    namespace = "com.sedsoftware.blinktracker"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "com.sedsoftware.blinktracker"
+        minSdk = 24
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.3"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+    implementation(platform(libs.compose.bom))
+
+    implementation(libs.ark.mvikotlin.core)
+    implementation(libs.ark.mvikotlin.main)
+    implementation(libs.ark.mvikotlin.extensions.coroutines)
+    implementation(libs.ark.decompose.core)
+    implementation(libs.ark.decompose.extensions)
+    implementation(libs.ark.essenty)
+
+    implementation(libs.core.ktx)
+    implementation(libs.lifecycle.runtime.ktx)
+    implementation(libs.datastore)
+    implementation(libs.activity.compose)
+    implementation(libs.ui)
+    implementation(libs.ui.graphics)
+    implementation(libs.ui.tooling.preview)
+    implementation(libs.material3)
+
+    debugImplementation(libs.ui.tooling)
+}
