8c24cea93cf6c03c186c441b425194d05660017e
DJ_Kovrik <djkovrik@gmail.com>
2023-06-10 15:28:21 +0300

    Implement tracking controls

--- app/src/main/java/com/sedsoftware/blinktracker/ui/component/TrackingControls.kt
+++ app/src/main/java/com/sedsoftware/blinktracker/ui/component/TrackingControls.kt
@@ -1,13 +1,169 @@
 package com.sedsoftware.blinktracker.ui.component
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CropFree
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.Stop
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedIconButton
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.sedsoftware.blinktracker.R.string
 import com.sedsoftware.blinktracker.components.tracker.BlinkTracker
+import com.sedsoftware.blinktracker.ui.PreviewStubs
+import com.sedsoftware.blinktracker.ui.theme.BlinkTrackerTheme
 
 @Composable
 fun TrackingControls(
-    tracker: BlinkTracker.Model,
+    model: BlinkTracker.Model,
     modifier: Modifier = Modifier,
+    onStartClick: () -> Unit = {},
+    onStopClick: () -> Unit = {},
+    onMinimizeClick: () -> Unit = {},
+    onSettingsClick: () -> Unit = {},
 ) {
+    Row(modifier = modifier.fillMaxWidth()) {
+        // Start/Stop Minimize
+        Row(modifier = Modifier.weight(1f)) {
+            Button(
+                onClick = if (model.isTrackingActive) onStopClick else onStartClick,
+                contentPadding = PaddingValues(horizontal = 8.dp, vertical = 2.dp),
+                modifier = Modifier
+                    .padding(horizontal = 4.dp),
+            ) {
+                if (model.isTrackingActive) {
+                    Icon(
+                        imageVector = Icons.Default.Stop,
+                        contentDescription = "Stop",
+                        tint = MaterialTheme.colorScheme.onPrimary,
+                        modifier = Modifier
+                    )
+                    Text(
+                        text = stringResource(id = string.button_stop),
+                        style = MaterialTheme.typography.bodySmall,
+                        color = MaterialTheme.colorScheme.onPrimary,
+                        modifier = Modifier.padding(horizontal = 4.dp),
+                    )
+                } else {
+                    Icon(
+                        imageVector = Icons.Default.PlayArrow,
+                        contentDescription = "Start",
+                        tint = MaterialTheme.colorScheme.onPrimary,
+                        modifier = Modifier
+                    )
+                    Text(
+                        text = stringResource(id = string.button_start),
+                        style = MaterialTheme.typography.bodySmall,
+                        color = MaterialTheme.colorScheme.onPrimary,
+                        modifier = Modifier.padding(horizontal = 4.dp),
+                    )
+                }
+            }
 
+            OutlinedButton(
+                onClick = onMinimizeClick,
+                contentPadding = PaddingValues(horizontal = 8.dp, vertical = 2.dp),
+                border = BorderStroke(
+                    width = 1.dp,
+                    color = MaterialTheme.colorScheme.primary
+                ),
+                modifier = Modifier.padding(horizontal = 4.dp),
+            ) {
+                Icon(
+                    imageVector = Icons.Default.CropFree,
+                    contentDescription = "Minimize",
+                    tint = MaterialTheme.colorScheme.primary,
+                    modifier = Modifier
+                )
+                Text(
+                    text = stringResource(id = string.button_minimize),
+                    style = MaterialTheme.typography.bodySmall,
+                    color = MaterialTheme.colorScheme.primary,
+                    modifier = Modifier.padding(horizontal = 4.dp),
+                )
+            }
+        }
+
+        // Settings
+        OutlinedIconButton(
+            onClick = onSettingsClick,
+            enabled = !model.isTrackingActive,
+            border = BorderStroke(
+                width = 1.dp,
+                color = MaterialTheme.colorScheme.primary
+            ),
+            modifier = Modifier,
+        ) {
+            Icon(
+                imageVector = Icons.Default.Settings,
+                contentDescription = "Settings",
+                tint = MaterialTheme.colorScheme.primary,
+                modifier = Modifier
+                    .alpha(alpha = if (model.isTrackingActive) 0.5f else 1f)
+            )
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun PreviewPreferencesNotActiveLight() {
+    BlinkTrackerTheme(darkTheme = false) {
+        Surface {
+            TrackingControls(
+                model = PreviewStubs.trackerNotActiveWithFace,
+            )
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun PreviewPreferencesActiveLight() {
+    BlinkTrackerTheme(darkTheme = false) {
+        Surface {
+            TrackingControls(
+                model = PreviewStubs.trackerActiveWithFace,
+            )
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun PreviewPreferencesNotActiveDark() {
+    BlinkTrackerTheme(darkTheme = true) {
+        Surface {
+            TrackingControls(
+                model = PreviewStubs.trackerNotActiveWithFace,
+            )
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun PreviewPreferencesActiveDark() {
+    BlinkTrackerTheme(darkTheme = true) {
+        Surface {
+            TrackingControls(
+                model = PreviewStubs.trackerActiveWithFace,
+            )
+        }
+    }
 }
