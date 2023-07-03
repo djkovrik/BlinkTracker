3a7bad59a7470a4508f8fb6cbfe4d3ae347c9626
DJ_Kovrik <djkovrik@gmail.com>
2023-07-03 19:55:59 +0300

    Release 1.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.sedsoftware.blinktracker"
         minSdk = 26
         targetSdk = 33
-        versionCode = 100101
-        versionName = "1.1.1"
+        versionCode = 100200
+        versionName = "1.2.0"
         setProperty("archivesBaseName", applicationId)
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
