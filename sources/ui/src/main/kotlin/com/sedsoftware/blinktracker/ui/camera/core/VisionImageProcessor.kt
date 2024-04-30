c2f0e0369b6cf445675be9a33cfd510a2ec56b7e
Michael Hsieh <mdhsieh8@gmail.com>
2024-04-30 14:34:56 -0700

    Move white color into constant.

--- sources/ui/src/main/kotlin/com/sedsoftware/blinktracker/ui/camera/core/VisionImageProcessor.kt
+++ sources/ui/src/main/kotlin/com/sedsoftware/blinktracker/ui/camera/core/VisionImageProcessor.kt
@@ -43,6 +43,7 @@ class FaceDetectorProcessor(detectorOptions: FaceDetectorOptions) : VisionImageP
 
     private var lastAnalyzedTimestamp = 0L
     private val lowLightThreshold = 105.0
+    private val whiteHexValue = 0xFF
 
     override val faceData: Flow<VisionFaceData>
         get() = _faceData
@@ -60,7 +61,7 @@ class FaceDetectorProcessor(detectorOptions: FaceDetectorOptions) : VisionImageP
             TimeUnit.SECONDS.toMillis(1)) {
             val buffer = image.planes[0].buffer
             val data = buffer.toByteArray()
-            val pixels = data.map { it.toInt() and 0xFF }
+            val pixels = data.map { it.toInt() and whiteHexValue }
             val luma = pixels.average()
             lastAnalyzedTimestamp = currentTimestamp
 
